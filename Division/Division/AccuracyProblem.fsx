592692eaca45a811faf62b80ea2b9c2816f0f927
Marvin Laucher <marv070@gmail.com>
2017-12-01 09:33:18 -0500

    create envelope.png using loops and conditionals in fsharp

--- Division/Division/AccuracyProblem.fsx
+++ Division/Division/AccuracyProblem.fsx
@@ -1,1 +1,10 @@
-﻿
+﻿open System.Drawing
+open System.IO
+
+let pathAndFileName
+    = Path.Combine("/Users/marvinlaucher/FsDev/Division", "accuracy.png")
+
+let bitmap = new Bitmap(32,32)
+
+
+bitmap.Save(pathAndFileName)
--- Division/Division/Script.fsx
+++ Division/Division/Script.fsx
@@ -1,5 +1,38 @@
-﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
-// for more guidance on F# programming.
+﻿let naiveLine (x0,y0) (x1,y1) color (bitmap:Bitmap) =
+    printfn "%i" x0
+    printfn "%i" y0
+    printfn " -^-^-^-^-^-^-^"
+    
+    let xLen = float (x1-x0)
+    let yLen = float (y1-y0)
 
-#load "Component1.fs"
-open Division
+    if xLen <> 0.0 then
+        for x in x0..x1 do
+            let proportion = float (x-x0) / xLen
+            let y = (int(Math.Round(proportion * yLen))) + y0
+            //printfn  "look here "
+            printfn "%i" y 
+            bitmap.SetPixel(x,y, color)
+
+
+    if yLen <> 0.0 then
+         for y in y0..y1 do
+             let proportion = float (y-y0) / yLen
+             let x = (int(Math.Round(proportion * xLen))) + x0
+             //printfn  "look here "
+             printfn "%i" x 
+             bitmap.SetPixel(x,y, color)
+
+let pathAndFileName
+    = Path.Combine("/Users/marvinlaucher/FsDev/Division", "naive.png")
+
+let bitmap = new Bitmap(32,32)
+
+naiveLine (3,7) (4,27)  Color.Red bitmap
+naiveLine (6,7) (10,27)  Color.Blue bitmap
+naiveLine (10,7) (13,20)  Color.Green bitmap
+naiveLine (13,7) (17,17)  Color.Orange bitmap
+naiveLine (16,7) (22,17)  Color.Black bitmap
+
+
+bitmap.Save(pathAndFileName)
