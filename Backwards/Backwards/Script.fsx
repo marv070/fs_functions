e251f1d321593539f065626f3000f78f11ef2dd7
Marvin Laucher <marv070@gmail.com>
2017-12-03 09:37:34 -0500

    use and update records in fsharp

--- Backwards/Backwards/Script.fsx
+++ Backwards/Backwards/Script.fsx
@@ -1,5 +1,51 @@
-﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
-// for more guidance on F# programming.
+﻿open System.Drawing
+open System.IO
+    
+let naiveLine (x0,y0) (x1,y1) color (bitmap:Bitmap) =
+    //printfn "%i" x0
+    //printfn "%i" y0
+    //printfn " -^-^-^-^-^-^-^"
+    
+    let xLen = float (x1-x0)
+    let yLen = float (y1-y0)
 
-#load "Component1.fs"
-open Backwards
+    let x0,y0,x1,y1 = if x0 > x1 then x1,y1,x0,y0 else x0,y0,x1,y1
+    if xLen <> 0.0 then
+        for x in x0..x1 do
+            let proportion = float (x-x0) / xLen
+            let y = (int(Math.Round(proportion * yLen))) + y0
+            //printfn  "look here "
+            printfn "%i" y 
+            bitmap.SetPixel(x,y, color)
+
+
+    let x0,y0,x1,y1 = if y0 > y1 then x1,y1,x0,y0 else x0,y0,x1,y1
+    if yLen <> 0.0 then
+         for y in y0..y1 do
+             let proportion = float (y-y0) / yLen
+             let x = (int(Math.Round(proportion * xLen))) + x0
+             //printfn  "look here "
+             printfn "%i" x 
+             bitmap.SetPixel(x,y, color)
+
+let pathAndFileName
+    = Path.Combine("/Users/marvinlaucher/FsDev/Backwards", "envelope.png")
+
+let bitmap = new Bitmap(39,27)
+
+let fill color (bitmap:Bitmap) = 
+    for x in 0..bitmap.Width-1 do
+        for y in 0..bitmap.Height-1 do 
+            bitmap.SetPixel(x,y, color)
+fill Color.LightSkyBlue bitmap
+
+naiveLine (0,0) (38,0)  Color.Navy bitmap
+naiveLine (38,0) (38,26)  Color.Navy bitmap
+naiveLine (0,0) (0,26)  Color.Navy bitmap
+naiveLine (1,1) (19,19)  Color.Navy bitmap
+naiveLine (20,18) (37,1)  Color.Navy bitmap
+naiveLine (0,26) (38,26)  Color.Navy bitmap
+
+
+
+bitmap.Save(pathAndFileName)
